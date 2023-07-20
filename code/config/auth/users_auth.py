96a05987b8b492749b6b34ad14254dd3c2ea56bb
Froilan Leiza <leizafroilan@gmail.com>
2023-07-21 03:17:33 +0800

    cleanup

--- code/config/auth/users_auth.py
+++ code/config/auth/users_auth.py
@@ -32,7 +32,7 @@ def authenticate_user(db: Session, username: str, password: str):
 
     return user
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
--- main.py
+++ main.py
@@ -14,6 +14,6 @@ app = FastAPI()
 app.include_router(pun_router)
 app.include_router(auth_router)
 
-if __name__ == "__main__":
-    print(os.getcwd())
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     print(os.getcwd())
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000)
