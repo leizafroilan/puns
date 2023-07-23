6421f4ba1163d59a5ac3e2f641d2ada125b3f3ad
Froilan Leiza <leizafroilan@gmail.com>
2023-07-24 01:11:02 +0800

    added corsmiddleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from code.routes.pun_route import router as pun_router
 from code.config.auth.users_route import router as auth_router
 
@@ -10,6 +11,14 @@ PunnyAPI. 🚀
 Embrace the pun-tastic possibilities and let the laughter flow freely with every call to our pun-filled API.
 """
 
+
+# Set up CORS middleware
+origins = [
+    "http://localhost:3000",  
+    "http://3.15.142.60:9000/"
+]
+
+
 app = FastAPI(
     title="PunnyAPI",
     description=description,
@@ -17,6 +26,14 @@ app = FastAPI(
     license_info={
         "name": "MIT License",
     },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
 )
 
 app.include_router(pun_router)
